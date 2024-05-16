badc14514a2051b2ec6117c4875dde8675a91075
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2024-05-16 16:02:54 +0500

    Added architecture.

---
+++ lib/features/auth/data/repositories/auth_repository_imp.dart
@@ -0,0 +1,34 @@
+import 'package:blog_app/core/errors/exceptions.dart';
+import 'package:blog_app/core/errors/failure.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImp implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+  const AuthRepositoryImp(this.authRemoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userId = await authRemoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
